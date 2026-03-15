513ef6da77723a55eb572db98f407620379834f3
nicolass0 <128260146+nicolass0@users.noreply.github.com>
2026-03-15 17:33:37 +0100

    Refactor authentication logic to use 'database' object

--- script.js
+++ script.js
@@ -1,37 +1,51 @@
-let fakeDB = {};
+let database={};
 
-function accetta(){
+function entra(){
 document.getElementById("warning").style.display="none";
-document.getElementById("auth").classList.remove("hidden");
+document.getElementById("register").classList.remove("hidden");
 }
 
 function esci(){
 document.body.innerHTML="<h1>ACCESSO NEGATO</h1>";
 }
 
+function vaiLogin(){
+document.getElementById("register").style.display="none";
+document.getElementById("login").classList.remove("hidden");
+}
+
 function register(){
+
 let name=document.getElementById("regName").value;
 let pass=document.getElementById("regPass").value;
 
-fakeDB[name]=pass;
+database[name]=pass;
 
 alert("Registrazione completata");
+
+vaiLogin();
 }
 
 function login(){
+
 let name=document.getElementById("logName").value;
 let pass=document.getElementById("logPass").value;
 
-if(fakeDB[name]===pass){
+if(database[name]===pass){
+
 startHack(name);
+
 }else{
-alert("Credenziali errate");
-}
+
+alert("Password errata");
+
 }
 
-function startHack(name){
+}
 
-document.getElementById("auth").style.display="none";
+function startHack(nome){
+
+document.getElementById("login").style.display="none";
 document.getElementById("hacker").classList.remove("hidden");
 
 let bar=document.getElementById("bar");
@@ -39,27 +53,27 @@ let status=document.getElementById("status");
 
 let progress=0;
 
-let interval=setInterval(()=>{
+let timer=setInterval(()=>{
 
 progress++;
 
 bar.style.width=progress+"%";
 
-status.innerText="Tracciamento soggetto: "+name+" "+progress+"%";
+status.innerText="Tracciamento soggetto: "+nome+" "+progress+"%";
 
 if(progress>=100){
 
-clearInterval(interval);
+clearInterval(timer);
 
 document.getElementById("hacker").style.display="none";
 document.getElementById("final").classList.remove("hidden");
 
 setTimeout(()=>{
 location.reload();
-},4000);
+},5000);
 
 }
 
-},80);
+},70);
 
 }
