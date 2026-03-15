5fbe9a7e715e1d78b490de772ef0125a02dba51d
nicolass0 <128260146+nicolass0@users.noreply.github.com>
2026-03-15 15:55:22 +0100

    Replace password reset logic with login form
    Removed password reset logic and replaced with a login form.

--- send_reset.php
+++ send_reset.php
@@ -1,26 +1,19 @@
-<?php
-if(isset($_POST['email'])){
-    $email = $_POST['email'];
+<!DOCTYPE html>
+<html lang="it">
+<head>
+  <meta charset="UTF-8">
+  <title>Login</title>
+</head>
+<body>
+  <h2>Login</h2>
+  <form action="login.php" method="POST">
+    <input type="email" name="email" placeholder="Email" required><br><br>
+    <input type="password" name="password" placeholder="Password" required><br><br>
+    <button type="submit">Accedi</button>
+  </form>
 
-    // Qui controlli se l'email è nel DB
-    // $user = SELECT * FROM utenti WHERE email = $email;
-
-    // Generi un token casuale
-    $token = bin2hex(random_bytes(16));
-
-    // Salvi il token nel DB associato all'utente con scadenza
-    // UPDATE utenti SET reset_token = $token, reset_expire = NOW() + INTERVAL 1 HOUR WHERE email = $email;
-
-    // Crei il link di reset
-    $link = "https://www.sarasux.com/reset_password.php?token=".$token;
-
-    // Invio email (usando mail() o servizi come SendGrid, Mailgun)
-    $subject = "Recupero password";
-    $message = "Clicca qui per reimpostare la password: ".$link;
-    $headers = "From: info@sarasux.com\r\n";
-
-    mail($email, $subject, $message, $headers);
-
-    echo "Se l'email è registrata, ti abbiamo inviato un link per reimpostare la password.";
-}
-?>
+  <p>
+    <a href="recupera_password.php">Hai dimenticato la password?</a>
+  </p>
+</body>
+</html>
