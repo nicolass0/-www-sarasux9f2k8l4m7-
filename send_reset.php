4c7cfab6b3d10a8c2ed6dd7ede2da54cb08bf71a
nicolass0 <128260146+nicolass0@users.noreply.github.com>
2026-03-15 15:51:01 +0100

    Implement password reset functionality

--- send_reset.php
+++ send_reset.php
@@ -1,1 +1,26 @@
+<?php
+if(isset($_POST['email'])){
+    $email = $_POST['email'];
 
+    // Qui controlli se l'email è nel DB
+    // $user = SELECT * FROM utenti WHERE email = $email;
+
+    // Generi un token casuale
+    $token = bin2hex(random_bytes(16));
+
+    // Salvi il token nel DB associato all'utente con scadenza
+    // UPDATE utenti SET reset_token = $token, reset_expire = NOW() + INTERVAL 1 HOUR WHERE email = $email;
+
+    // Crei il link di reset
+    $link = "https://www.sarasux.com/reset_password.php?token=".$token;
+
+    // Invio email (usando mail() o servizi come SendGrid, Mailgun)
+    $subject = "Recupero password";
+    $message = "Clicca qui per reimpostare la password: ".$link;
+    $headers = "From: info@sarasux.com\r\n";
+
+    mail($email, $subject, $message, $headers);
+
+    echo "Se l'email è registrata, ti abbiamo inviato un link per reimpostare la password.";
+}
+?>
